2c105d7de3a9bc7693027967861d18cff58a8699
Marcelo Costa <mesmacosta@gmail.com>
2021-02-04 20:25:42 -0300

    add fk

--- storage/v2_create_table_edges.sql
+++ storage/v2_create_table_edges.sql
@@ -1,5 +1,5 @@
 create table edges(      
-  n1 int,                -- node 1      
-  n2 int,                -- node 2         
-  primary key (n1,n2)  -- primary key      
+  n1 int REFERENCES nodes (id),    -- node 1      
+  n2 int REFERENCES nodes (id),    -- node 2         
+  primary key (n1,n2)              -- primary key      
 );      
