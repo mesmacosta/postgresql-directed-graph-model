9ece8cdf0a1622ab88602f7078009e983c33c0bf
Marcelo Costa <mesmacosta@gmail.com>
2021-02-04 20:14:08 -0300

    Update build-docker.sh

--- build-docker.sh
+++ build-docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # https://github.com/sameersbn/docker-postgresql
 
-docker pull sameersbn/postgresql:12-20200524
+# docker pull sameersbn/postgresql:12-20200524
 docker build -t sameersbn/postgresql github.com/sameersbn/docker-postgresql
 docker run --name postgresql -itd --restart always \
   --publish 5432:5432 \
